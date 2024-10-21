f3d2f0588791a418445723e31f1f84b484c12338
MoonSsunny <moonsun116@naver.com>
2024-10-21 18:01:55 +0900

    [feat] 로그인 validation 구현

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,22 +1,64 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 import { Link } from 'react-router-dom';
+import api from '../utils/api';
 
 const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!email) {
+      alert('이메일 입력은 필수입니다');
+      emailRef.current.focus();
+      return;
+    }
+    if (!password) {
+      alert('비밀번호를 입력해주세요.');
+      passwordRef.current.focus();
+      return;
+    }
+    try {
+      const params = {
+        email,
+        password,
+      };
+      const res = await api.post('/user/login', params);
+      console.log('res____', res);
+    } catch (error) {
+      console.error(error);
+      alert('로그인에 실패하였습니다.');
+    }
+  };
+
   return (
     <div className="display-center">
-      <Form className="login-box">
+      <Form className="login-box" onSubmit={handleSubmit}>
         <h1>로그인</h1>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={(event) => setEmail(event.target.value)}
+            ref={emailRef}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={(event) => setPassword(event.target.value)}
+            ref={passwordRef}
+          />
         </Form.Group>
         <div className="button-box">
           <Button type="submit" className="button-primary">
