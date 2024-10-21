367e02582c6424d340d25193c1c44825bcf95939
MoonSsunny <moonsun116@naver.com>
2024-10-21 23:30:42 +0900

    [feat] login 버튼 라우트 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import styles from './Header.module.scss';
 
 const Header = () => {
+  const navigate = useNavigate();
+  const handleLogin = () => {
+    navigate('/login');
+  };
   return (
     <header className={styles.header}>
       <img src="/logo-2.png" alt="logo" className={styles.logoImg} />
-      <button className={styles.loginBtn}>로그인</button>
+      <button className={styles.loginBtn} onClick={handleLogin}>
+        로그인
+      </button>
     </header>
   );
 };
