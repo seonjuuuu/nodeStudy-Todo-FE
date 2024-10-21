a18a552e4c8891d0088a1e4cb8ff4e0c4e079015
MoonSsunny <moonsun116@naver.com>
2024-10-21 21:11:52 +0900

    [feat] 날짜 추가 및 ui 변경

--- src/components/TodoBoard.js
+++ src/components/TodoBoard.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import TodoItem from './TodoItem';
 import styles from './TodoBoard.module.scss';
 
 const TodoBoard = ({ todoList, handleDeleteTask, handleUpdateTask }) => {
+  const [month, setMonth] = useState('');
+  const [day, setDay] = useState('');
+  const [year, setYear] = useState('');
+
+  useEffect(() => {
+    const today = new Date();
+    const year = today.getFullYear();
+    const monthNames = [
+      'JAN',
+      'FEB',
+      'MAR',
+      'APR',
+      'MAY',
+      'JUN',
+      'JUL',
+      'AUG',
+      'SEP',
+      'OCT',
+      'NOV',
+      'DEC',
+    ];
+    const month = monthNames[today.getMonth()];
+    const day = String(today.getDate()).padStart(2, '0');
+    setMonth(month);
+    setDay(day);
+    setYear(year);
+  }, []);
+
   return (
     <div className={styles.todoBoard}>
-      <h2>Todo List</h2>
+      <h2 className={styles.date}>
+        <span className={styles.day}>{day}</span>
+        <div className={styles.date2}>
+          <span className={styles.year}>{year}</span>
+          <span className={styles.month}>{month}</span>
+        </div>
+      </h2>
       {todoList.length > 0 ? (
         todoList.map((item, index) => (
           <TodoItem
